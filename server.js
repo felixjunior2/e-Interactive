b7f9cf9aed35731d7755a5ec6c182e9e7729a5ec
felix junior <166924832+felixjunior2@users.noreply.github.com>
2024-12-07 22:54:22 -1200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const InMemoryStorage = require('./storage'); // Classe de armazenamento
+
+// Importação das rotas
 const quizConfigRoutes = require('./quizConfigRoutes');
-const userDeploymentRoutes = require('./userDeploymentRoutes');
-const quizAnalyticsRoutes = require('./quizAnalyticsRoutes');
 
+// Inicialização do servidor
 const app = express();
-const storage = new InMemoryStorage();
-
-// Middleware
 app.use(bodyParser.json());
 
-// Rotas
-app.use('/quiz/config', quizConfigRoutes(storage));
-app.use('/quiz/user', userDeploymentRoutes(storage));
-app.use('/quiz/analytics', quizAnalyticsRoutes(storage));
+// Registro das rotas
+app.use('/quiz/config', quizConfigRoutes());
 
-// Servidor
+// Inicialização do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
